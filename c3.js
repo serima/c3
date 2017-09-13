090bab568919b9441ef4101feef7bc0437841bfd
Ryo Shibayama <j02521@gmail.com>
2017-09-13 09:42:39 +0900

    99

--- c3.js
+++ c3.js
@@ -92,7 +92,118 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 
 
+var asyncGenerator = function () {
+  function AwaitValue(value) {
+    this.value = value;
+  }
 
+  function AsyncGenerator(gen) {
+    var front, back;
+
+    function send(key, arg) {
+      return new Promise(function (resolve, reject) {
+        var request = {
+          key: key,
+          arg: arg,
+          resolve: resolve,
+          reject: reject,
+          next: null
+        };
+
+        if (back) {
+          back = back.next = request;
+        } else {
+          front = back = request;
+          resume(key, arg);
+        }
+      });
+    }
+
+    function resume(key, arg) {
+      try {
+        var result = gen[key](arg);
+        var value = result.value;
+
+        if (value instanceof AwaitValue) {
+          Promise.resolve(value.value).then(function (arg) {
+            resume("next", arg);
+          }, function (arg) {
+            resume("throw", arg);
+          });
+        } else {
+          settle(result.done ? "return" : "normal", result.value);
+        }
+      } catch (err) {
+        settle("throw", err);
+      }
+    }
+
+    function settle(type, value) {
+      switch (type) {
+        case "return":
+          front.resolve({
+            value: value,
+            done: true
+          });
+          break;
+
+        case "throw":
+          front.reject(value);
+          break;
+
+        default:
+          front.resolve({
+            value: value,
+            done: false
+          });
+          break;
+      }
+
+      front = front.next;
+
+      if (front) {
+        resume(front.key, front.arg);
+      } else {
+        back = null;
+      }
+    }
+
+    this._invoke = send;
+
+    if (typeof gen.return !== "function") {
+      this.return = undefined;
+    }
+  }
+
+  if (typeof Symbol === "function" && Symbol.asyncIterator) {
+    AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
+      return this;
+    };
+  }
+
+  AsyncGenerator.prototype.next = function (arg) {
+    return this._invoke("next", arg);
+  };
+
+  AsyncGenerator.prototype.throw = function (arg) {
+    return this._invoke("throw", arg);
+  };
+
+  AsyncGenerator.prototype.return = function (arg) {
+    return this._invoke("return", arg);
+  };
+
+  return {
+    wrap: function (fn) {
+      return function () {
+        return new AsyncGenerator(fn.apply(this, arguments));
+      };
+    },
+    await: function (value) {
+      return new AwaitValue(value);
+    }
+  };
+}();
 
 
 
@@ -4319,16 +4430,13 @@ c3_chart_fn.zoom.range = function (range) {
 
 c3_chart_internal_fn.initPie = function () {
     var $$ = this,
-        d3 = $$.d3,
-        config = $$.config;
+        d3 = $$.d3;
     $$.pie = d3.layout.pie().value(function (d) {
         return d.values.reduce(function (a, b) {
             return a + b.value;
         }, 0);
     });
-    if (!config.data_order) {
-        $$.pie.sort(null);
-    }
+    $$.pie.sort($$.getOrderFunction() || null);
 };
 
 c3_chart_internal_fn.updateRadius = function () {
@@ -5809,26 +5917,36 @@ c3_chart_internal_fn.isOrderAsc = function () {
     var config = this.config;
     return typeof config.data_order === 'string' && config.data_order.toLowerCase() === 'asc';
 };
-c3_chart_internal_fn.orderTargets = function (targets) {
+c3_chart_internal_fn.getOrderFunction = function () {
     var $$ = this,
         config = $$.config,
         orderAsc = $$.isOrderAsc(),
         orderDesc = $$.isOrderDesc();
     if (orderAsc || orderDesc) {
-        targets.sort(function (t1, t2) {
+        return function (t1, t2) {
             var reducer = function reducer(p, c) {
                 return p + Math.abs(c.value);
             };
             var t1Sum = t1.values.reduce(reducer, 0),
                 t2Sum = t2.values.reduce(reducer, 0);
-            return orderAsc ? t2Sum - t1Sum : t1Sum - t2Sum;
-        });
+            return orderDesc ? t2Sum - t1Sum : t1Sum - t2Sum;
+        };
     } else if (isFunction(config.data_order)) {
-        targets.sort(config.data_order);
+        return config.data_order;
     } else if (isArray(config.data_order)) {
-        targets.sort(function (t1, t2) {
-            return config.data_order.indexOf(t1.id) - config.data_order.indexOf(t2.id);
-        });
+        var order = config.data_order;
+        return function (t1, t2) {
+            return order.indexOf(t1.id) - order.indexOf(t2.id);
+        };
+    }
+};
+c3_chart_internal_fn.orderTargets = function (targets) {
+    var fct = this.getOrderFunction();
+    if (fct) {
+        targets.sort(fct);
+        if (this.isOrderAsc() || this.isOrderDesc()) {
+            targets.reverse();
+        }
     }
     return targets;
 };
@@ -8203,7 +8321,7 @@ c3_chart_internal_fn.lineWithRegions = function (d, x, y, _regions) {
                     s += sWithRegion(d[i - 1], d[i], j, diff);
                 }
             }
-        prev = d[i].x;
+        
     }
 
     return s;
@@ -8352,7 +8470,11 @@ c3_chart_internal_fn.pointR = function (d) {
 c3_chart_internal_fn.pointExpandedR = function (d) {
     var $$ = this,
         config = $$.config;
-    return config.point_focus_expand_enabled ? config.point_focus_expand_r ? config.point_focus_expand_r : $$.pointR(d) * 1.75 : $$.pointR(d);
+    if (config.point_focus_expand_enabled) {
+        return isFunction(config.point_focus_expand_r) ? config.point_focus_expand_r(d) : config.point_focus_expand_r ? config.point_focus_expand_r : $$.pointR(d) * 1.75;
+    } else {
+        return $$.pointR(d);
+    }
 };
 c3_chart_internal_fn.pointSelectR = function (d) {
     var $$ = this,
